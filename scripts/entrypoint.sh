1bf41b4b59057fa9b8de4f1cd4ebc99cb93df8c8
Caio Duque <caioduque.dev@gmail.com>
2025-04-14 22:58:10 -0300

    update script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-while ! nc -z ${DB_HOST} ${DB_PORT}; do
-  echo "Waiting for PostgreSQL..."
-  sleep 1
-done
+#while ! nc -z ${DB_HOST} ${DB_PORT}; do
+#  echo "Waiting for PostgreSQL..."
+#  sleep 1
+#done
 
 echo "✅ Postgres Database Started Successfully ($DB_HOST:$DB_PORT)"
 
+uv run app/manage.py migrate
 uv run app/manage.py runserver 0.0.0.0:8000
