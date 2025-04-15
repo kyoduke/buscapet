c7ec641ece85e8108c723d4b55f83d98af3f825b
Caio Duque <caioduque.dev@gmail.com>
2025-04-14 22:55:13 -0300

    more updates

--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def homepage(request):
-    return HttpResponse("Home")
+    return render(request, "core/homepage.html", {})
